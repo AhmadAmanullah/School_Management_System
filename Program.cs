c60a73744691a983545b88ad283a5e4fd083ebeb
Ahmad Amanullah <119287157+AhmadAmanullah@users.noreply.github.com>
2024-10-20 03:57:13 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+namespace SchoolManagementSystem
+{
+    class Program
+    {
+        public int managementCase;
+
+        static void Main(string[] args)
+        {
+            Program schoolManager = new schoolManager();
+            //SMS may anay wala kon hy?
+            WriteLine("Enter 0 for updating student information.\n" +
+                "Enter 1 for updating teacher information.\n" +
+                "Enter 2 for updating class information.\n");
+            schoolManager.managementCase = Convert.ToInt32(ReadLine());
+
+            switch (schoolManager.managementCase)
+            {
+                case 0:
+                    Student student_0 = new("John", 45, 5);
+                    student_0.StudentInformation();
+                    break;
+                case 1:
+                    //teacher case
+                    break;
+                case 2:
+                    //teacher case
+                    break;
+                case 3:
+                    //teacher case
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace SchoolManagementSystem
+{
+    internal class Student
+    {
+        string studentName = "defaultValue";
+        int studentID;
+        int studentClassNumber;
+
+        public Student(string _studentName, int _studentID, int _studentClassNumber) 
+        {
+            studentName = _studentName;
+            studentID = _studentID;
+            studentClassNumber = _studentClassNumber;
+        }
+
+        public void StudentInformation()
+        {
+            WriteLine("studentName = " + studentName + 
+                "\nStudent RollNumber = " + studentID +
+                "\nstudentClassNumber = " + studentClassNumber);
+        }
+    }
+}
