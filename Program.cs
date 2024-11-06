ac1eedd8021dd9c92a53b52fd21b530ebff33a26
Ahmad Amanullah <119287157+AhmadAmanullah@users.noreply.github.com>
2024-11-07 01:27:29 +0500

    Student Basic Add, View, Update and Delete data from text file.

--- Program.cs
+++ Program.cs
@@ -1,38 +1,109 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace SchoolManagementSystem
 {
     class Program
     {
-        public int managementCase;
+        int accessDatabase = 0;
+        int managementCase;
+        int accessStudentDatabase = 0;
+        int studentCase;
+        bool IsValidStudentTask;
 
         static void Main(string[] args)
         {
-            Program schoolManager = new schoolManager();
-            //SMS may anay wala kon hy?
-            WriteLine("Enter 0 for updating student information.\n" +
-                "Enter 1 for updating teacher information.\n" +
-                "Enter 2 for updating class information.\n");
-            schoolManager.managementCase = Convert.ToInt32(ReadLine());
-
-            switch (schoolManager.managementCase)
+            Program schoolManager = new Program();
+            while (schoolManager.accessDatabase == 0)
             {
-                case 0:
-                    Student student_0 = new("John", 45, 5);
-                    student_0.StudentInformation();
-                    break;
-                case 1:
-                    //teacher case
-                    break;
-                case 2:
-                    //teacher case
-                    break;
-                case 3:
-                    //teacher case
-                    break;
-                default:
-                    break;
+                WriteLine("Please select which database you want to access:");
+                WriteLine("0. student database.\n" +
+                    "1. teacher database.\n" +
+                    "2. class database.\n" +
+                    "3. subject database.\n" +
+                    "4. attendance database.\n" +
+                    "5. examination database.\n" +
+                    "6. result database.\n" +
+                    "7. timetable database.\n" +
+                    "8. fee database.\n" +
+                    "9. library database.\n" +
+                    "10. user database.\n" +
+                    "11. authentication and authorization database.\n" +
+                    "12. report database.\n");
+                schoolManager.managementCase = Convert.ToInt32(ReadLine());
+                switch (schoolManager.managementCase)
+                {
+                    case 0:
+                        // Display available tasks to the user
+                        while (schoolManager.accessStudentDatabase == 0)
+                        {
+
+                            Console.WriteLine("Please select which task you want to perform:\n" +
+                                "0. Add Student\n" +
+                                "1. View Student\n" +
+                                "2. Update Student\n" +
+                                "3. Delete Student\n");
+                            schoolManager.studentCase = Convert.ToInt32(ReadLine());
+                            Student studentObj = new Student();
+                            switch (schoolManager.studentCase)
+                            {
+                                case 0:
+                                    // Add student
+                                    Console.WriteLine("Adding student Data...");
+                                    studentObj.CreateStudentFile();
+                                    studentObj.AddStudentInformation();
+                                    studentObj.AddDataInStudentFile();
+                                    break;
+                                case 1:
+                                    // View student
+                                    Console.WriteLine("Viewing student Data...");
+                                    studentObj.ViewStudentFileData();
+                                    break;
+                                case 2:
+                                    // Update student
+                                    Console.WriteLine("Updating student Data...");
+                                    studentObj.UpdateStudentDataFromFile();
+                                    break;
+                                case 3:
+                                    // Delete student
+                                    Console.WriteLine("Deleting student Data...");
+                                    studentObj.DeleteStudentDataFromFile();
+                                    // Implement your delete student logic here
+                                    break;
+                                default:
+                                    Console.WriteLine("Invalid student task selected. Please enter valid student task.");
+                                    schoolManager.studentCase = Convert.ToInt32(ReadLine());
+                                    break;
+
+                            }
+                            WriteLine("Enter 0 to continue access to student database or 1 to go back");
+                            schoolManager.accessStudentDatabase = Convert.ToInt32(ReadLine());
+                        }
+                        break;
+                    case 1:
+                        // Teacher case
+
+                        break;
+                    case 2:
+                        // Class case
+
+                        break;
+                    case 3:
+                        // Subject case
+
+                        break;
+                    default:
+                        WriteLine("Invalid database selected. Please enter valid database number.");
+                        schoolManager.managementCase = Convert.ToInt32(ReadLine());
+                        break;
+
+                }
+                WriteLine("Enter 0 to continue access to database or 1 to exit");
+                schoolManager.accessDatabase = Convert.ToInt32(ReadLine());
+
             }
+            WriteLine("\nPress any key to exit...");
+            ReadKey();
         }
     }
 }
--- Student.cs
+++ Student.cs
@@ -4,28 +4,229 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 using static System.Console;
-
+using System.IO;
 namespace SchoolManagementSystem
 {
     internal class Student
     {
-        string studentName = "defaultValue";
-        int studentID;
-        int studentClassNumber;
+        string name = "DefaultName";
+        string ID;
+        string grade;
+        string dateOfBirth;
+        string CNIC;
+        string fatherName = "DefaultFatherName";
+        string StudentDataFile = @"D:\.Net Data\Console Applications\SchoolManagementSystem\StudentData.txt";
 
-        public Student(string _studentName, int _studentID, int _studentClassNumber) 
+        public void CreateStudentFile()
         {
-            studentName = _studentName;
-            studentID = _studentID;
-            studentClassNumber = _studentClassNumber;
+            if (File.Exists(StudentDataFile))
+            {
+                WriteLine("File Already Created.");
+            }
+            else
+            {
+                // Create a new file     
+                FileStream fs = File.Create(StudentDataFile);
+            }
         }
 
-        public void StudentInformation()
+        public void AddStudentInformation()
         {
-            WriteLine("studentName = " + studentName + 
-                "\nStudent RollNumber = " + studentID +
-                "\nstudentClassNumber = " + studentClassNumber);
+            WriteLine("Enter Student Name");
+            name = ReadLine();
+
+            //check user entered name
+            while (!IsStringAndAlphabetic(name))
+            {
+                WriteLine("Invalid input! Please enter the name again.");
+                name = ReadLine();
+            }
+            //check user ID
+            WriteLine("Please enter ID");
+            ID = ReadLine();
+
+            while (!IsNumericValueEntered(ID))
+            {
+                WriteLine("Invalid input! Please enter a valid ID in numbers.");
+                ID = ReadLine();
+            }
+            //check user grade
+            WriteLine("Enter Student Grade");
+            grade = ReadLine();
+
+            while (!IsNumericValueEntered(grade))
+            {
+                WriteLine("Invalid input! Please enter a valid grade in numbers.");
+                grade = ReadLine();
+            }
+            ///check user dateofbirth
+            WriteLine("Enter Student dateOfBirth");
+            dateOfBirth = ReadLine();
+
+            while (!IsNumericValueEntered(dateOfBirth))
+            {
+                WriteLine("Invalid input! Please enter a valid date of birth in numbers.");
+                dateOfBirth = ReadLine();
+            }
+
+            ///check user CNIC
+            WriteLine("Enter Student CNIC");
+            CNIC = ReadLine();
+
+            while (!IsNumericValueEntered(CNIC))
+            {
+                WriteLine("Invalid input! Please enter a valid CNIC in numbers.");
+                CNIC = ReadLine();
+            }
+
+            ///check user FatherName
+            WriteLine("Enter Student FatherName");     
+            fatherName = ReadLine();
+
+
+            while (!IsStringAndAlphabetic(fatherName))
+            {
+                WriteLine("Invalid input! Please enter the father name again.");
+                fatherName = ReadLine();
+            }
+        }
+
+        public void AddDataInStudentFile()
+        {
+
+            File.AppendAllText(StudentDataFile, name);
+            File.AppendAllText(StudentDataFile, "\n");
+
+            File.AppendAllText(StudentDataFile, ID);
+            File.AppendAllText(StudentDataFile, "\n");
+
+            File.AppendAllText(StudentDataFile, grade);
+            File.AppendAllText(StudentDataFile, "\n");
+
+            File.AppendAllText(StudentDataFile, dateOfBirth);
+            File.AppendAllText(StudentDataFile, "\n");
+
+            File.AppendAllText(StudentDataFile, CNIC);
+            File.AppendAllText(StudentDataFile, "\n");
+
+            File.AppendAllText(StudentDataFile, fatherName);
+            File.AppendAllText(StudentDataFile, "\n\n");
+        }
+
+        bool IsStringAndAlphabetic(string name)
+        {
+            foreach (char character in name)
+            {
+                if (!char.IsLetter(character))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        bool IsNumericValueEntered(string _numericValue)
+        {
+            return int.TryParse(_numericValue, out _);  // The out _ discards the parsed integer value
+        }
+
+
+
+        public void ViewStudentInformation()
+        {
+            WriteLine("studentName = " + name +
+                "\nStudent ID = " + ID +
+                "\nStudent Grade = " + grade +
+                "\nStudent DateOfBirth = " + dateOfBirth +
+                "\nStudent CNIC = " + CNIC +
+                "\nStudent Father Name = " + fatherName);
+        }
+
+        public void ViewStudentFileData()
+        {
+            string StudentInfo = File.ReadAllText(StudentDataFile, Encoding.UTF8);
+            WriteLine(StudentInfo);
+        }
+
+        public void UpdateStudentDataFromFile()
+        {
+            WriteLine("Enter Student Name To Change");
+            string updateName = ReadLine();
+            while (!IsStringAndAlphabetic(updateName))
+            {
+                WriteLine("Invalid input! Please enter the name again.");
+                updateName = ReadLine();
+            }
+
+            WriteLine("Enter the new name to replace it with:");
+            string newName = ReadLine();
+            while (!IsStringAndAlphabetic(newName))
+            {
+                WriteLine("Invalid input! Please enter the name again.");
+                newName = ReadLine();
+            }
+
+            string[] fileLines = File.ReadAllLines(StudentDataFile, Encoding.UTF8);
+            bool found = false;
+
+            for (int i = 0; i < fileLines.Length; i++)
+            {
+                if (fileLines[i].Trim().Equals(updateName.Trim(), StringComparison.OrdinalIgnoreCase))
+                {
+                    fileLines[i] = newName; // Replace the found name with the new name
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                // Write the modified lines back to the file
+                File.WriteAllLines(StudentDataFile, fileLines, Encoding.UTF8);
+                WriteLine("Student Name Updated Successfully.");
+            }
+            else
+            {
+                WriteLine("Student Name Not Found.");
+            }
+        }
+
+
+        public void DeleteStudentDataFromFile()
+        {
+            WriteLine("Enter Student Name To delete");
+            string deleteName = ReadLine();
+            while (!IsStringAndAlphabetic(deleteName))
+            {
+                WriteLine("Invalid input! Please enter the name again.");
+                deleteName = ReadLine();
+            }
+
+            string[] fileLines = File.ReadAllLines(StudentDataFile, Encoding.UTF8);
+            bool found = false;
+
+            for (int i = 0; i < fileLines.Length; i++)
+            {
+                if (fileLines[i].Trim().Equals(deleteName.Trim(), StringComparison.OrdinalIgnoreCase))
+                {
+                    fileLines[i] = ""; // delete the found name with the new name
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                // Write the modified lines back to the file
+                File.WriteAllLines(StudentDataFile, fileLines, Encoding.UTF8);
+                WriteLine("Student Name deleted Successfully.");
+            }
+            else
+            {
+                WriteLine("Student Name Not Found.");
+            }
         }
     }
 }
